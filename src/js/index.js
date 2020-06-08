cf4b4637cfe3d44e280038e8c3298b22f6889bf5
alexuser <alexsaf91@mail.ru>
2020-06-08 19:06:35 +0300

    webpack & scss

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+// styles
+import "../scss/main.scss"
+
+
+
+// scripts
+import "../js/scripts"
+
+
+// favicons
+import "../img/favicons/android-icon-192x192.png"
+import "../img/favicons/apple-icon-57x57.png"
+import "../img/favicons/apple-icon-60x60.png"
+import "../img/favicons/apple-icon-72x72.png"
+import "../img/favicons/apple-icon-76x76.png"
+import "../img/favicons/apple-icon-114x114.png"
+import "../img/favicons/apple-icon-120x120.png"
+import "../img/favicons/apple-icon-144x144.png"
+import "../img/favicons/apple-icon-152x152.png"
+import "../img/favicons/apple-icon-180x180.png"
+import "../img/favicons/apple-icon-precomposed.png"
+import "../img/favicons/apple-icon.png"
+import "../img/favicons/favicon-16x16.png"
+import "../img/favicons/favicon-32x32.png"
+import "../img/favicons/favicon-96x96.png"
+import "../img/favicons/ms-icon-144x144.png"
+
+
